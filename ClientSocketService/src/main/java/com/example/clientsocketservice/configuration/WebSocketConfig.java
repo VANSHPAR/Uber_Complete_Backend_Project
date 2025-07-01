7e488cb30fbacb7cc9dc9f44544a087e7fc46dcc
VANSHPAR <159923891+VANSHPAR@users.noreply.github.com>
2025-07-01 20:55:36 +0530

    Add files via upload

--- ClientSocketService/src/main/java/com/example/clientsocketservice/configuration/WebSocketConfig.java
+++ ClientSocketService/src/main/java/com/example/clientsocketservice/configuration/WebSocketConfig.java
@@ -2,13 +2,11 @@ package com.example.clientsocketservice.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocket
+@EnableWebSocketMessageBroker
 public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
--- ClientSocketService/src/main/java/com/example/clientsocketservice/controller/TestController.java
+++ ClientSocketService/src/main/java/com/example/clientsocketservice/controller/TestController.java
@@ -1,15 +1,24 @@
 package com.example.clientsocketservice.controller;
 
 import com.example.clientsocketservice.dto.TestResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @Controller
 public class TestController {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    public TestController(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
 
     @MessageMapping("/ping")
     @SendTo("/topic/ping")
@@ -21,8 +30,10 @@ public class TestController {
 
     @SendTo("/topic/scheduled")
     @Scheduled(fixedRate = 2000)
-    public String sendPeriodicMessage() {
-        return "Periodic message from server "+System.currentTimeMillis();
+    public void sendPeriodicMessage() {
+        System.out.println("Executed periodic function");
+        simpMessagingTemplate.convertAndSend("/topic/scheduled", "Periodic message from server "+System.currentTimeMillis());
+
     }
 
 
